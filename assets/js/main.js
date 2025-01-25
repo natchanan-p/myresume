680199bf3a66a1ed5f71c047b4963aea3ff73208
Natchanan <vannatchanan@gmail.com>
2025-01-26 01:19:16 +0700

    update animation & css

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,9 +13,7 @@
   const birthYear = new Date('10/04/1996').getFullYear() + 543;
   document.getElementById('age').textContent = currentYear - birthYear;
 
-  const experienceYear = new Date().getFullYear() - new Date().getFullYear('01/07/2017').getFullYear();
-  console.log({experienceYear});
-  
+  const experienceYear = new Date().getFullYear() - new Date('01/07/2017').getFullYear();
   document.getElementById('experience').textContent = experienceYear;
 
   /**
